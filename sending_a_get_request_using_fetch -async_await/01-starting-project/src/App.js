db385c0f3beef0baea9689ef6fb945825b6fe8dd
anupshaw <shawanup871@gmail.com>
2022-12-09 00:22:07 +0530

    second commit

--- sending_a_get_request_using_fetch -async_await/01-starting-project/src/App.js
+++ sending_a_get_request_using_fetch -async_await/01-starting-project/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,16 +8,18 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async function () {
     setIsLoading(true);
-    setError(null)
+    setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
+      console.log(response)
       if (!response.ok) {
-        throw new Error("Something is wrong");
+        throw new Error("Something went wrong");
       }
 
       const data = await response.json();
+      console.log(data)
 
       const transformedMovies = data.results.map((movieData) => {
         return {
@@ -34,7 +36,11 @@ function App() {
       setError(error.message);
       setIsLoading(false);
     }
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   return (
     <React.Fragment>
